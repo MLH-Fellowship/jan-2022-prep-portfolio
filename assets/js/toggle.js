8cc2b8f87d28dcfdb4cb7f8fddfc1f643bfa4246
yyjlincoln <yyjlincoln@gmail.com>
2022-01-11 19:50:49 +1100

    Feat: Complete the basic features of dark more; added auto mode; added lots of adaptive classess.

--- assets/js/toggle.js
+++ assets/js/toggle.js
@@ -1,23 +1,85 @@
-var mode = document.getElementById("dark-theme-toggle");
+const darkModeButton = document.getElementById("dark-theme-toggle");
+const darkModeStatusIcon = document.getElementById("dark-mode-status-icon");
+const darkModeStatusIndicator = document.getElementById("dark-mode-status-indicator");
+// Mapping:
+// 0 - auto
+// 1 - dark
+// 2 - light
+var currentMode = 0;
+const modeMapping = {
+  0: "auto",
+  1: "dark",
+  2: "light"
+}
+
 window.addEventListener("load", function () {
   initTheme();
-
-  if (mode !== null) {
-    mode.addEventListener("change", function () {
-      resetTheme();
-    });
-  }
 });
 
 function initTheme() {
-  var darkThemeSelected =
-    localStorage.getItem("mode") !== null &&
-    localStorage.getItem("mode") === "dark";
+  const modePreference = localStorage.getItem("mode") ?? "auto"
+  updateTheme(modePreference, true)
+  darkModeButton.addEventListener("click", () => {
+    currentMode = (currentMode + 1) % 3
+    updateTheme(modeMapping[currentMode])
+  })
+}
 
-  if (mode !== null) mode.checked = darkThemeSelected;
-  darkThemeSelected
-    ? document.body.setAttribute("theme", "dark")
-    : document.body.removeAttribute("theme");
+function updateTheme(modePreference, isInit = false) {
+  var isDark = false
+  // Store the updated theme.
+  // This should be done before switch - ref: default case
+  localStorage.setItem("mode", modePreference)
+  switch (modePreference) {
+    case "auto":
+      isDark = window.matchMedia && window.matchMedia(' (prefers-color-scheme: dark)').matches
+      currentMode = 0
+      break
+    case "dark":
+      isDark = true
+      currentMode = 1
+      break
+    case "light":
+      isDark = false
+      currentMode = 2
+      break;
+    default:
+      // Invalid state, clear mode preference and reset to auto
+      localStorage.setItem("mode", null)
+      isDark = false
+  }
+
+  // Change the button text
+  darkModeStatusIndicator.innerText = modePreference
+
+  beforeModeChange({ isDark, modePreference }).then(() => {
+    isDark ? applyDarkMode() : applyLightMode()
+    // Remove the hidden tag - the hidden tag was added 
+    // to avoid flashing when switching pages in dark mode.
+    if (isInit) {
+      document.body.classList.remove("hidden")
+    }
+  }).then(() => {
+    afterModeChange({ isDark, modePreference })
+  })
+}
+
+async function beforeModeChange({ isDark, modePreference }) {
+  // [TODO] Set up the animation
+  return
+}
+
+async function afterModeChange({ isDark, modePreference }) {
+  // [TODO] Clean up the animation
+  return
+}
+
+function applyDarkMode() {
+  document.body.setAttribute("theme", "dark")
+}
+
+function applyLightMode() {
+  document.body.removeAttribute("theme");
 }
 
 function resetTheme() {
