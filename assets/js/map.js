97622e2c427d825bf98579a05451e31bd21b7d67
ditsuke <ditsuke@protonmail.com>
2022-01-15 22:16:36 +0530

    Fix styling and cleanup dead data

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([17.6078,8.0817], 3);
+var map = L.map('map').setView([17.6078, 8.0817], 3);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
@@ -7,20 +7,10 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 var fellows = [
-    // {
-    //  lat:51.5,
-    //  long:-0.09,
-    //  pic:'aaron.jpg',
-    //     name: 'Aaron',
-    //     location: 'XYZ',
-    //     github: '/',
-    //     linkedin: '/',
-    //     role: 'Fellow',
-    // },
     {
-        lat:-14.2350,
-        long:-51.9253,
-        pic:'gabriel.jpg',
+        lat: -14.2350,
+        long: -51.9253,
+        pic: 'gabriel.jpg',
         name: 'Gabriel Cruz',
         location: 'Brazil',
         github: 'https://github.com/gmelodie',
@@ -28,9 +18,9 @@ var fellows = [
         role: 'Pod Leader',
     },
     {
-        lat:42.4215,
-        long:-75.6972,
-        pic:'Kish.jpeg',
+        lat: 42.4215,
+        long: -75.6972,
+        pic: 'Kish.jpeg',
         name: 'Kish',
         location: 'Ottawa, Canada',
         github: 'http://github.com/kishdubey',
@@ -38,9 +28,9 @@ var fellows = [
         role: 'Fellow',
     },
     {
-        lat:34.0151,
-        long:71.5249,
-        pic:'nomani.jpg',
+        lat: 34.0151,
+        long: 71.5249,
+        pic: 'nomani.jpg',
         name: 'Khudadad Nomani',
         location: 'Peshawar, Pakistan',
         github: 'https://github.com/KhudaDad414',
@@ -48,9 +38,9 @@ var fellows = [
         role: 'Fellow',
     },
     {
-        lat:19.3150,
-        long:84.7941,
-        pic:'pritish.jpeg',
+        lat: 19.3150,
+        long: 84.7941,
+        pic: 'pritish.jpeg',
         name: 'Pritish Samal',
         location: 'Berhampur, India',
         github: 'https://github.com/CIPHERTron',
@@ -58,29 +48,19 @@ var fellows = [
         role: 'Fellow',
     },
     {
-        lat:13.0827,
-        long:80.2707,
-        pic:'vaibhave.jpg',
+        lat: 13.0827,
+        long: 80.2707,
+        pic: 'vaibhave.jpg',
         name: 'Vaibhave S',
         location: 'Chennai, India',
         github: 'https://github.com/VaibhaveS',
         linkedin: 'https://www.linkedin.com/in/s-vaibhave-davey-a8798118b/',
         role: 'Fellow',
     },
-    // {
-    //     lat:28.6139,
-    //     long:77.2090,
-    //     pic:'tushar.png',
-    //     name: 'Tushar Malik',
-    //     location: 'New Delhi, India',
-    //     github: 'https://github.com/ditsuke',
-    //     linkedin: '/',
-    //     role: 'Fellow',
-    // },
     {
-        lat:-33.8688,
-        long:151.2093,
-        pic:'yan.jpg',
+        lat: -33.8688,
+        long: 151.2093,
+        pic: 'yan.jpg',
         name: 'Yijun Yan',
         location: 'Sydney, Australia',
         github: 'https://github.com/yyjlincoln',
@@ -88,9 +68,9 @@ var fellows = [
         role: 'Fellow',
     },
     {
-        lat:28.7041,
-        long:77.1025,
-        pic:'riya.png',
+        lat: 28.7041,
+        long: 77.1025,
+        pic: 'riya.png',
         name: 'Riya Dev Varshney',
         location: 'Delhi, India',
         github: 'https://github.com/Riyadevvarshney11',
@@ -98,9 +78,9 @@ var fellows = [
         role: 'Fellow',
     },
     {
-        lat:28.4595,
-        long:77.0266,
-        pic:'harsh.png',
+        lat: 28.4595,
+        long: 77.0266,
+        pic: 'harsh.png',
         name: 'Harsh Sharma',
         location: 'Gurgaon, India',
         github: 'https://github.com/harshsharma6401',
@@ -108,9 +88,9 @@ var fellows = [
         role: 'Fellow',
     },
     {
-        lat:19.0760,
-        long:72.8777,
-        pic:'dhruv.jpg',
+        lat: 19.0760,
+        long: 72.8777,
+        pic: 'dhruv.jpg',
         name: 'Dhruv Sachdev',
         location: 'Mumbai, India',
         github: 'https://github.com/Dhruv-Sachdev1313',
@@ -118,9 +98,9 @@ var fellows = [
         role: 'Fellow',
     },
     {
-        lat:23.5204,
-        long:87.3119,
-        pic:'pal.jpg',
+        lat: 23.5204,
+        long: 87.3119,
+        pic: 'pal.jpg',
         name: 'Aniket Pal',
         location: 'Durgapur, India',
         github: 'https://github.com/Aniket762',
@@ -128,9 +108,9 @@ var fellows = [
         role: 'Fellow',
     },
     {
-        lat:28.4817,
-        long:77.1873,
-        pic:'anubhav.jpg',
+        lat: 28.4817,
+        long: 77.1873,
+        pic: 'anubhav.jpg',
         name: 'Anubhav Gupta',
         location: 'New Delhi, India',
         github: 'https://github.com/anubhav06',
@@ -138,9 +118,9 @@ var fellows = [
         role: 'Fellow',
     },
     {
-        lat:16.7488,
-        long:78.0035,
-        pic:'Sahithi.jpeg',
+        lat: 16.7488,
+        long: 78.0035,
+        pic: 'Sahithi.jpeg',
         name: 'P.Sahithi Reddy',
         location: 'Mahabubnagar, India',
         github: 'https://github.com/psahithireddy',
@@ -148,9 +128,9 @@ var fellows = [
         role: 'Fellow',
     },
     {
-        lat:31.6340,
-        long:74.8723,
-        pic:'advitiay.jpg',
+        lat: 31.6340,
+        long: 74.8723,
+        pic: 'advitiay.jpg',
         name: 'Advitiay Anand',
         location: 'Amritsar, India',
         github: 'https://github.com/adizcode',
@@ -158,9 +138,9 @@ var fellows = [
         role: 'Fellow',
     },
     {
-        lat:19.0330,
-        long:73.0297,
-        pic:'kshitij.jpg',
+        lat: 19.0330,
+        long: 73.0297,
+        pic: 'kshitij.jpg',
         name: 'Kshitij Darekar',
         location: 'Navi Mumbai, India',
         github: 'https://github.com/KshitijDarekar',
@@ -168,9 +148,9 @@ var fellows = [
         role: 'Fellow',
     },
     {
-        lat:42.2181,
+        lat: 42.2181,
         long: -70.9410,
-        pic:'emeka.jpg',
+        pic: 'emeka.jpg',
         name: 'Emeka',
         location: 'Weymouth, United States',
         github: 'https://github.com/Chukwuemeka-Mba',
@@ -178,9 +158,9 @@ var fellows = [
         role: 'Fellow',
     },
     {
-        lat:28.6139391,
+        lat: 28.6139391,
         long: 77.2090212,
-        pic:'tushar.png',
+        pic: 'tushar.png',
         name: 'Tushar',
         location: 'New Delhi, India',
         github: 'https://github.com/ditsuke',
@@ -189,23 +169,23 @@ var fellows = [
     },
 ];
 
-function median(Fellows){
-    let lat=0.0,long=0.0
-    for(var i=0;i<Fellows.length;i++){
-        lat+=Fellows[i].lat;
-        long+=Fellows[i].long;
+function median(Fellows) {
+    let lat = 0.0, long = 0.0
+    for (var i = 0; i < Fellows.length; i++) {
+        lat += Fellows[i].lat;
+        long += Fellows[i].long;
     }
-    lat/=Fellows.length;
-    long/=Fellows.length;
-    return [lat,long];
+    lat /= Fellows.length;
+    long /= Fellows.length;
+    return [lat, long];
 }
 
-function distance(Fellow1,Fellow2) {
-    var lat1=Fellow1.lat,lat2=Fellow2.lat, lon1=Fellow1.long, lon2=Fellow2.long
+function distance(Fellow1, Fellow2) {
+    var lat1 = Fellow1.lat, lat2 = Fellow2.lat, lon1 = Fellow1.long, lon2 = Fellow2.long
     // The math module contains a function
     // named toRadians which converts from
     // degrees to radians.
-    lon1 =  lon1 * Math.PI / 180;
+    lon1 = lon1 * Math.PI / 180;
     lon2 = lon2 * Math.PI / 180;
     lat1 = lat1 * Math.PI / 180;
     lat2 = lat2 * Math.PI / 180;
@@ -213,34 +193,34 @@ function distance(Fellow1,Fellow2) {
     let dlon = lon2 - lon1;
     let dlat = lat2 - lat1;
     let a = Math.pow(Math.sin(dlat / 2), 2)
-             + Math.cos(lat1) * Math.cos(lat2)
-             * Math.pow(Math.sin(dlon / 2),2);
+        + Math.cos(lat1) * Math.cos(lat2)
+        * Math.pow(Math.sin(dlon / 2), 2);
     let c = 2 * Math.asin(Math.sqrt(a));
     // Radius of earth in kilometers. Use 3956
     // for miles
     let r = 6371;
     // calculate the result
-    return(c * r);
+    return (c * r);
 }
 
-for(var i=0;i<fellows.length;i++){
-    var nearestIndex=-1,nearestDistance=Number.MAX_SAFE_INTEGER;
-    for(var j=0;j<fellows.length;j++){
-        if(i==j) continue;
-        if(distance(fellows[i],fellows[j])<nearestDistance){
-            nearestDistance=distance(fellows[i],fellows[j]);
-            nearestIndex=j;
+for (var i = 0; i < fellows.length; i++) {
+    var nearestIndex = -1, nearestDistance = Number.MAX_SAFE_INTEGER;
+    for (var j = 0; j < fellows.length; j++) {
+        if (i == j) continue;
+        if (distance(fellows[i], fellows[j]) < nearestDistance) {
+            nearestDistance = distance(fellows[i], fellows[j]);
+            nearestIndex = j;
         }
     }
-    fellows[i].neighbour=fellows[nearestIndex].name
+    fellows[i].neighbour = fellows[nearestIndex].name
 }
 
-for(var i=0;i<fellows.length;i++){
+for (var i = 0; i < fellows.length; i++) {
     var icon = L.icon({
-        iconUrl: '/assets/img/'+fellows[i].pic,
+        iconUrl: '/assets/img/' + fellows[i].pic,
         iconSize: [50, 50], // size of the icon
         // iconAnchor: [22, 94],
-        popupAnchor:  [0, -16], // point from which the popup should open relative to the iconAnchor
+        popupAnchor: [0, -16], // point from which the popup should open relative to the iconAnchor
         className: 'marker'
     });
     var popup = `<div class="container">
@@ -252,33 +232,33 @@ for(var i=0;i<fellows.length;i++){
                 <a href=${fellows[i].github}><em class="fab fa-github"></em></a>
                 </div>
                 </div>`
-    var marker = L.marker([fellows[i].lat, fellows[i].long],{icon:icon}).addTo(map);
+    var marker = L.marker([fellows[i].lat, fellows[i].long], {icon: icon}).addTo(map);
     marker.bindPopup(popup);
 }
 
 
-let Medobj=median(fellows);
+let Medobj = median(fellows);
 
 var icon = L.icon({
-        iconUrl: '/assets/img/tea.jpeg',
-        iconSize:     [40, 40], // size of the icon
-        popupAnchor:  [0, -16], // point from which the popup should open relative to the iconAnchor
-        className: 'marker'
+    iconUrl: '/assets/img/tea.jpeg',
+    iconSize: [40, 40], // size of the icon
+    popupAnchor: [0, -16], // point from which the popup should open relative to the iconAnchor
+    className: 'marker'
 });
 var popup = `<div class="container">
             <h6 style='display:inline'><b>They call me median ;)</b><h7 style='display:inline'>, A perfect place to hangout!</h7></h6>
             <hr/><b>Oman</b>
             </div>`
-var marker = L.marker([Medobj[0], Medobj[1]],{icon:icon}).addTo(map);
+var marker = L.marker([Medobj[0], Medobj[1]], {icon: icon}).addTo(map);
 marker.bindPopup(popup);
 
 // Set the visitor's pin on the map on demand
 const setVisitorPin = () => {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(
-            function(position) {
+            function (position) {
                 // On success..Get current cordinates.
-                let circle = L.circle([position.coords.latitude,position.coords.longitude], {
+                let circle = L.circle([position.coords.latitude, position.coords.longitude], {
                     color: 'black',
                     fillColor: '#f03',
                     fillOpacity: 0.5,
@@ -286,7 +266,7 @@ const setVisitorPin = () => {
                 }).addTo(map);
                 circle.bindPopup("You are here!").openPopup();
             },
-            function(error) {
+            function (error) {
                 // On error code..Do nothing
             },
             {timeout: 30000, enableHighAccuracy: true, maximumAge: 75000}
